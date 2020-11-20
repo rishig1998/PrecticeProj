eff36c58dfc4e73681ca7aad06e41a36b2486c29
HP <rishig1998@gmail.com>
2020-11-20 14:23:52 +0530

    lession-2

---
+++ app/src/main/java/com/example/practicepro/Nextpage.kt
@@ -0,0 +1,11 @@
+package com.example.practicepro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Nextpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nextpage)
+    }
+}
