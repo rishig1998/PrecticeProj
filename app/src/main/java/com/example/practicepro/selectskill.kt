c02578f0b6273ebed4a32d7b26480597d62ed1cb
HP <rishig1998@gmail.com>
2020-11-20 21:39:07 +0530

    lession-5

--- app/src/main/java/com/example/practicepro/selectskill.kt
+++ app/src/main/java/com/example/practicepro/selectskill.kt
@@ -1,19 +1,13 @@
 package com.example.practicepro
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class Nextpage : lifecycle() {
+class selectskill : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nextpage)
+        setContentView(R.layout.activity_selectskill)
     }
 
-    fun btn_next(view: View)
-    {
-        val next=Intent(this,selectskill::class.java)
-        startActivity(next)
-    }
 }
