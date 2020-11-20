27493c474efb4fcb89724ddd80a96cde7975de87
HP <rishig1998@gmail.com>
2020-11-20 14:58:24 +0530

    lession-3

--- app/src/main/java/com/example/practicepro/MainActivity.kt
+++ app/src/main/java/com/example/practicepro/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.practicepro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       start_btn.setOnClickListener {
+           val i=Intent(this,Nextpage::class.java)
+           startActivity(i)
+
+       }
     }
 }
